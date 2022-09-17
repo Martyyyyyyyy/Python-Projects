c41cd4c1daf24ddf4374ecde5390e034c73f67c6
Martin Fesenko <Mfesenko2003@gmail.com>
2022-09-17 12:24:09 +0300

    first project

---
+++ small projects/block-letters.py
@@ -0,0 +1,60 @@
+# initials' program
+S = """ 
+BBBB
+B   B
+B   B
+BBBB     
+B   B 
+B   B
+BBBB    
+"""
+L = """
+L
+L
+L
+L
+L
+L
+LLLLL
+"""
+print(S + L)
+# My initials
+M = """
+M   M
+MM MM
+MM MM
+M M M
+M   M 
+M   M 
+M   M
+"""
+F = """
+FFFFF
+F
+F
+FFF
+F
+F
+F
+"""
+print(M)
+print(F)
+
+# also some snowman
+print("      *     *                    ")
+print("                      *          ")
+print("*         *                 *    ")
+print("     *           *     *         ")
+print("           HHHHHHH        *      ")
+print(" *         HHHHHHH               ")
+print("     *   HHHHHHHHHHH    *     *  ")
+print("         *   @ @   *             ")
+print("        *     ^     *            ")
+print("  *       *       *         *    ")
+print("         SSS * * S               ")
+print("         * SSSSSSSS *            ")
+print("       *      O   SS  *          ")
+print("      *       O    S   *         ")
+print("      *       O        *         ")
+print("       *              *          ")
+print("           *   *   *             ")
