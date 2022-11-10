09b5e0c8d50fcfdbe61f1126602d89e53d85179f
Martin Fesenko <Mfesenko2003@gmail.com>
2022-11-10 21:53:03 +0200

    fixing bugs

--- math projects/numbers.py
+++ math projects/numbers.py
@@ -1,7 +1,32 @@
 import random
+import secrets
+import string
 
-a = 20
+letters = string.ascii_letters
+digits = string.digits
+special_chars = string.punctuation
 
-for i in range(20):
-  b = random.randrange(10)
-  print(b) 
+alphabet = letters + digits + special_chars
+
+pwd_length = 12
+
+pwd = ''
+for i in range(pwd_length):
+  pwd += ''.join(secrets.choice(alphabet))
+
+print(pwd)
+
+while True:
+  pwd = ''
+  for i in range(pwd_length):
+    pwd += ''.join(secrets.choice(alphabet))
+
+  if (any(char in special_chars for char in pwd) and 
+      sum(char in digits for char in pwd)>=2):
+          break
+print(pwd)
+
+papers = ['1', '2', '3']
+
+q_list = [random.choice(papers) for i in range(40)]
+print(q_list)
