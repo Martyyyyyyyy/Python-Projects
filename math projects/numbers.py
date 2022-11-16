cacffeb62e1ea472b931e7432f29d4cc6d222fe2
Martin Fesenko <Mfesenko2003@gmail.com>
2022-11-16 17:54:01 +0200

    updates

--- math projects/numbers.py
+++ math projects/numbers.py
@@ -1,32 +1,11 @@
 import random
-import secrets
-import string
 
-letters = string.ascii_letters
-digits = string.digits
-special_chars = string.punctuation
+bound = 30
 
-alphabet = letters + digits + special_chars
-
-pwd_length = 12
-
-pwd = ''
-for i in range(pwd_length):
-  pwd += ''.join(secrets.choice(alphabet))
-
-print(pwd)
-
-while True:
-  pwd = ''
-  for i in range(pwd_length):
-    pwd += ''.join(secrets.choice(alphabet))
-
-  if (any(char in special_chars for char in pwd) and 
-      sum(char in digits for char in pwd)>=2):
-          break
-print(pwd)
-
-papers = ['1', '2', '3']
-
-q_list = [random.choice(papers) for i in range(40)]
-print(q_list)
+counter = 0
+if (counter == bound):
+  for i in range(10):
+    for j in range(10):
+      for k in range(10):
+        counter += 1
+        print(i, j, k)
